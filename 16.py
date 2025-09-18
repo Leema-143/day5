190958f5a1b3962b632dffabb109dbcef2c3d2a4
Leema-143 <crajalakshmi3066@gmail.com>
2025-09-18 21:30:25 +0530

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,10 @@
+# Python program to add a key:value pair to dictionary 
+dict = {'100':'sowji', '102':'lalitha'}  
+print("Current Dict is:", dict)  
+# using the subscript notation  
+# Dictionary_Name[New_Key_Name] = New_Key_Value  
+dict['104'] = 'ambika' 
+dict['105'] = 'anitha' 
+dict['106'] = 'lavanya' 
+dict['107'] = 'poornima' 
+print("Updated Dict is:", dict)
