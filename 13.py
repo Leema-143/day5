98473616b3e9729f4bd281e713da1830cb4b6bb2
Leema-143 <crajalakshmi3066@gmail.com>
2025-09-18 21:32:44 +0530

    Add files via upload

---
+++ 13.py
@@ -0,0 +1,6 @@
+ 
+test_tup1 = (1, 3, 5) 
+test_tup2 = (4, 6) 
+print("The original tuple 1 : " ,test_tup1) 
+print("The original tuple 2 : " ,test_tup2) 
+print("The tuple after concatenation is : ",test_tup1 + test_tup2) 
